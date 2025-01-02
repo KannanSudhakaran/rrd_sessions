20c95aa65e0adb658f33d9799cdb74d44b62d616
kannan <kannan.mumbai@yahoo.com>
2025-01-02 11:58:26 +0530

    secondcommit

--- day17/HelloWorldApp/HelloWorldApp/Hello.cs
+++ day17/HelloWorldApp/HelloWorldApp/Hello.cs
@@ -5,6 +5,7 @@ class Hello
     static void Main() //hardcoded entry point
     {
         System.Console.WriteLine("Hello, RRD folks welcome to C#!");
+        System.Console.WriteLine("End of main");
         return ; 
     }
 
