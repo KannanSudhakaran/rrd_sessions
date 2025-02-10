6c1e02f01c88a3ae5bab8fc5fddefdaff5469895
kannan <kannan.mumbai@yahoo.com>
2025-02-10 11:58:18 +0530

    'day41'

---
+++ day41/OCPsolution/AxisBankFDApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using AxisBankFDApp.Policy;
+using FDCoreLibrary.Models;
+
+namespace AxisBankFDApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fd1 = new FixedDeposit(101, "abc", 10000, 10, () => {
+
+                return .9;
+            });
+            Console.WriteLine(fd1.SimpleInterest);
+
+        }
+    }
+}
