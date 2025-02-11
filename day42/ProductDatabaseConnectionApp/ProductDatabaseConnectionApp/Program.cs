bb3b3479a6eee8d3961f44e1f11a12f76c303888
kannan <kannan.mumbai@yahoo.com>
2025-02-11 12:07:15 +0530

    'day42'

---
+++ day42/ProductDatabaseConnectionApp/ProductDatabaseConnectionApp/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+namespace ProductDatabaseConnectionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //   CaseStudy1();
+
+            CaseStudy2();
+
+        }
+
+        private static void CaseStudy2()
+        {
+            var connectionString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true;TrustServerCertificate=true";
+            var con = new SqlConnection(connectionString);
+            var cmd = new SqlCommand("select * from" +
+                " product", con);
+            con.Open();
+            var reader= cmd.ExecuteReader();
+            while (reader.Read()) {
+                Console.WriteLine(reader["name"]);
+                Console.WriteLine(reader["quantity"]);
+                Console.WriteLine(reader["price"]);
+
+            }
+            con.Close();
+        }
+
+        private static void CaseStudy1()
+        {
+
+
+            var connectionString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true;TrustServerCertificate=true";
+            var con = new SqlConnection(connectionString);
+            con.Open();
+            Console.WriteLine("connection opened connected to ");
+            Console.WriteLine(con.Database);
+            Console.WriteLine("conneciton state is ");
+            Console.WriteLine(con.State);
+            con.Close();
+
+        }
+    }
+}
